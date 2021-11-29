bb640cfe2ca2d4778e04485625908c103dc56229
Didriksson <mattias.didriksson@gmail.com>
2021-11-29 16:29:45 +0100

    Delete index.ts

--- index.ts
+++
@@ -1,6 +0,0 @@
-var argv = require('minimist')(process.argv.slice(2));
-
-console.log(`Running with arguments: ${argv._}`);
-const daySolution = require("./solutions/0/day_0");
-console.log(daySolution);
-console.log(daySolution.solve("1\\n2\\2"));
